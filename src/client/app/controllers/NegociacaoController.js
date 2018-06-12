8f3e0981c10efe01ba44a8af53db467cd4c94ed0
Fabricio Nogueira <nogsantos@gmail.com>
2018-06-11 21:13:01 -0300

    fix: tratamento para a data

--- src/client/app/controllers/NegociacaoController.js
+++ src/client/app/controllers/NegociacaoController.js
@@ -4,6 +4,11 @@
  * @class NegociacaoController
  */
 class NegociacaoController {
+	/**
+	 * Creates an instance of NegociacaoController.
+	 *
+	 * @memberof NegociacaoController
+	 */
 	constructor() {
 		let $ = document.querySelector.bind(document);
 
@@ -11,23 +16,43 @@ class NegociacaoController {
 		this.valor = $('#valor');
 		this.quantidade = $('#quantidade');
 
-		this.data.focus();
-		// this.data.value = `${new Date().getDate()}/${new Date().getMonth() + 1}/${new Date().getFullYear()}`;
+		this.setaData();
+		this.valor.focus();
 	}
 	/**
+	 * Seta data inicial
 	 *
+	 * @memberof NegociacaoController
+	 */
+	setaData() {
+		let day = new Date().getDate();
+		let month = new Date().getMonth() + 1;
+		let year = new Date().getFullYear();
+
+		this.data.value = `${day <= 9 ? '0' + day : day}/${month <= 9 ? '0' + month : month}/${year}`;
+	}
+	/**
+	 * Adiciona um evento
 	 *
 	 * @param {*} event
 	 * @memberof NegociacaoController
 	 */
 	adiciona(event) {
 		event.preventDefault();
-		let arrData = this.data.value;
-		let splData = arrData.split('/');
-		let dt = new Date(splData.map((item, indice) => item - (indice % 2)));
-
-		// let negociacao = new Negociacao(data, parseInt(this.quantidade.value), parseFloat(this.valor.value));
-
-		console.log(dt);
+		let negociacao = new Negociacao(this.formataData(this.data.value), parseInt(this.quantidade.value), parseFloat(this.valor.value));
+		console.log(negociacao);
+	}
+	/**
+	 * Formata data para cadastro
+	 *
+	 * @memberof NegociacaoController
+	 */
+	formataData(dataIn) {
+		if (!dataIn) {
+			return new Date();
+		}
+		let splData = dataIn.split('/');
+		let maping = splData.map((item, indice) => parseInt(item) - (indice % 2));
+		return new Date(...maping.reverse());
 	}
 }
